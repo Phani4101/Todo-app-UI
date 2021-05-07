a2dd30f7577dce2b990cfb02c5c2cee97c3a9a73
phani kumar pilla <chiruaditya208@gmail.com>
2021-05-07 10:40:43 +0530

    todo list ui

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var e=document;
+
+
+function func()
+{
+    e.querySelector("#todo-container").className = "blurs";
+    e.querySelector("#popup").className = "popups";
+}
+function func1()
+{
+    e.querySelector("#todo-container").className = "blurs";
+    e.querySelector("#popup1").className = "popups";
+}
